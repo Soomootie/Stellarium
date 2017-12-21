d3e7ffcf63fbdb36b67f741cdd5539010faf7c62
Soomootie <soomootie@gmail.com>
2017-12-21 16:10:51 +0100

    Add binance, and run some test, NEED API KEYS

--- src/Test.py
+++ src/Test.py
@@ -2,6 +2,9 @@ import time
 
 from bittrex.bittrex import Bittrex, API_V1_1
 from poloniex import Poloniex
+from binance.client import Client
+
+import Keys
 import datetime
 
 API = API_V1_1
@@ -10,24 +13,43 @@ TRADE = ''
 POLONIEX = 'POLONIEX'
 BITTREX = 'BITTREX'
 FORMAT = '{0:.8f}'
+_MARGIN = 0.00000001
 
-def poloniex(market = 'BTC_STR'):
+
+def poloniex(market = 'BTC_STR') :
     polo = Poloniex();
     ticket_poloniex = polo.returnTicker()[market]
-    print(ticket_poloniex)
+    print("Ask : " + FORMAT.format(ticket_poloniex['lowestAsk'])
+          + " Bid : " + FORMAT.format(ticket_poloniex['highestBid']))
     return float(ticket_poloniex['lowestAsk']) , float(ticket_poloniex['highestBid'])
     
-def bittrex(market = 'BTC-XLM'):
+def bittrex(market = 'BTC-XLM') :
     global API
     bittrex = Bittrex(None, None, api_version = API)
     ticket_bittrex = bittrex.get_ticker(market)['result']
-    print(FORMAT.format(ticket_bittrex))
+    print("Ask : " + FORMAT.format(ticket_bittrex['Ask'])
+          + " Bid : " + FORMAT.format(ticket_bittrex['Bid']))
     ask_Bittrex = ticket_bittrex['Ask']
     bid_Bittrex = ticket_bittrex['Bid']
     return ask_Bittrex, bid_Bittrex
 
-def main():
-    global TRADE, POLONIEX, BITTREX, DATE
+def _buy(_file, ask, bid, margin, trader) :
+    global TRADE, POLONIEX, BITTREX
+    _trader = POLONIEX if trader == BITTREX else BITTREX 
+    if TRADE == '' or TRADE != trader :
+        TRADE = trader
+        _file.write("ASK_" + trader + " : " + str(ask)
+                + " BID_" + BITTREX + " : " + str(bid) 
+                    + ' with : ' + FORMAT.format(margin) + "\n")
+
+
+def duration(_file, start) :
+    end = datetime.datetime.now()
+    duration = end - start
+    _file.write('END AT : ' + str(end) + '\nDuring ' + str(duration) + '\n\n')
+
+def main() :
+    global TRADE, POLONIEX, BITTREX, DATE, _MARGIN
     
     period = 1 
     MARGIN = 6
@@ -42,12 +64,16 @@ def main():
     while TIMES > 0 :
         ask_Poloniex , bid_Poloniex = poloniex()
         ask_Bittrex , bid_Bittrex = bittrex()
-        
-        
+        client = Client(Keys.publicKey(), Keys.privateKey())
+        tickers = client.get_orderbook_tickers()
+        for i in tickers :
+            if i['symbol'] == 'XLMBTC' :
+                print(i['askPrice'] + " " + i['bidPrice'])
+                break
         for i in range(MARGIN, MARGIN*3, ICR) :
-            I = i * 0.00000001
-            BUY_ON_POLONIEX = ask_Poloniex - bid_Bittrex + i * 0.00000001
-            BUY_ON_BITTREX = ask_Bittrex - bid_Poloniex + i * 0.00000001
+            I = i * _MARGIN
+            BUY_ON_POLONIEX = ask_Poloniex - bid_Bittrex + I
+            BUY_ON_BITTREX = ask_Bittrex - bid_Poloniex + I
             
             if BUY_ON_BITTREX < 0 or BUY_ON_POLONIEX < 0 :
                 print('POLONIEX : ' + FORMAT.format(BUY_ON_POLONIEX) + ' with : ' + FORMAT.format(I))
@@ -55,18 +81,10 @@ def main():
            
             if BUY_ON_POLONIEX < 0 :
                 print("TRY POLONIEX")
-                #if TRADE == '' or TRADE != POLONIEX :
-                print("WRITE POLONIEX")
-                TRADE = POLONIEX
-                FILE.write("ASK_POLONIEX : " + str(ask_Poloniex)
-                           + " BID_BITTREX : " + str(bid_Bittrex) + ' with : ' + FORMAT.format(I) + "\n")
+                _buy(FILE, ask_Poloniex, bid_Bittrex, I, POLONIEX)
             elif BUY_ON_BITTREX < 0 :
                 print("TRY BITTREX")
-                #if TRADE == '' or TRADE != BITTREX :
-                print("WRITE BITTREX")
-                TRADE = BITTREX 
-                FILE.write("ASK_BITTREX : " + str(ask_Bittrex)
-                           + " BID_POLONIEX : " + str(bid_Poloniex) + ' with : ' + FORMAT.format(I) + "\n")
+                _buy(FILE, ask_Bittrex, bid_Poloniex, I, BITTREX)
 
         if BUY_ON_BITTREX < 0 or BUY_ON_POLONIEX < 0 :
             FILE.write("\n")
@@ -74,8 +92,7 @@ def main():
         TIMES -= 1
         time.sleep(period)
     
-    END = datetime.datetime.now()    
-    FILE.write('\nEND AT ' + str(END) + '\n\n')
+    duration(FILE, DATE)
     FILE.close()
     
 if __name__ == "__main__":
